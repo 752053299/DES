e812c19c06b4b817b9a88abb9d2c2b73545ef10d
752053299 <752053299@qq.com>
2017-11-26 14:02:57 +0800

    加密软件

---
+++ DESTest/EncryptClass.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace DESTest
+{
+    class EncryptClass
+    {
+        private static byte[] Keys = { 0x15, 0x24, 0x5A, 0x7E, 0x98, 0xA5, 0x3D, 0xFF };//自定义密匙
+     
+
+        /// <summary>
+        /// 文件加密
+        /// </summary>
+        /// <param name="inFile">文件储存路径</param>
+        /// <param name="outFile">储存文件复制的路径</param>
+        /// <param name="encryptKey"></param>
+        /// <returns></returns>
+        public static bool EncryptDES(string inFile, string outFile, string encryptKey)
+        {
+            byte[] rgb = Keys;
+            try
+            {
+                byte[] rgbKeys = Encoding.UTF8.GetBytes(encryptKey.Substring(0, 8));
+                FileStream inFs = new FileStream(inFile, FileMode.Open, FileAccess.Read);//读入流
+                FileStream outFs = new FileStream(outFile, FileMode.OpenOrCreate, FileAccess.Write);// 等待写入流
+                outFs.SetLength(0);//帮助读写的变量
+                byte[] byteIn = new byte[1024*1024*5];//放临时读入的流
+                long readLen = 0;//读入流的长度
+                long totalLen = inFs.Length;//读入流的总长度
+                int everylen = 0;//每次读入流的长度
+                DES des = new DESCryptoServiceProvider();//将inFile加密后放到outFile
+                CryptoStream encStream = new CryptoStream(outFs, des.CreateEncryptor(rgb, rgbKeys), CryptoStreamMode.Write);
+                while (readLen < totalLen)
+                {
+                    everylen = inFs.Read(byteIn, 0, byteIn.Length);
+                    encStream.Write(byteIn, 0, everylen);
+                    readLen = readLen + everylen;
+                }
+                encStream.Close();
+                inFs.Close();
+                outFs.Close();
+                return true;//加密成功
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return false;//加密失败
+            }
+        }
+
+        public static bool DecryptDES(string inFile, string outFile, string encryptKey)
+        {
+            byte[] rgb = Keys;
+            try
+            {
+                byte[] rgbKeys = Encoding.UTF8.GetBytes(encryptKey.Substring(0, 8));
+                FileStream inFs = new FileStream(inFile, FileMode.Open, FileAccess.Read);//读入流
+                FileStream outFs = new FileStream(outFile, FileMode.OpenOrCreate, FileAccess.Write);// 等待写入流
+                outFs.SetLength(0);//帮助读写的变量
+                byte[] byteIn = new byte[1024 * 1024 * 5];//放临时读入的流
+                long readLen = 0;//读入流的长度
+                long totalLen = inFs.Length;//读入流的总长度
+                int everylen = 0;//每次读入流的长度
+                DES des = new DESCryptoServiceProvider();//将inFile加密后放到outFile
+                CryptoStream encStream = new CryptoStream(outFs, des.CreateDecryptor(rgb, rgbKeys), CryptoStreamMode.Write);
+                while (readLen < totalLen)
+                {
+                    everylen = inFs.Read(byteIn, 0, byteIn.Length);
+                    encStream.Write(byteIn, 0, everylen);
+                    readLen = readLen + everylen;
+                }
+                encStream.Close();
+                inFs.Close();
+                outFs.Close();
+                return true;//解密成功
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return false;//解密失败
+            }
+        }
+
+        ///// <summary>
+        ///// 拷贝文件
+        ///// </summary>
+        //public void copyFile()
+        //{
+        //    filePathA = this.fei.PostedFile.FileName;//获取文件全部路径
+        //    string fileName = this.fei.FileName;
+        //    string path = System.IO.Path.GetDirectoryName(filePathA);
+        //    filePathB = path + "\\1" + fileName;//重新设置文件名
+        //    File.Copy(filePathA, filePathB);
+        //}
+    }
+}
--- DESTest/Form1.cs
+++ DESTest/Form1.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Security.Cryptography;
 using System.Text;
 using System.Windows.Forms;
 
@@ -11,9 +14,102 @@ namespace DESTest
 {
     public partial class Form1 : Form
     {
+        string OutFile;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            string SourceFilePath = @textBox1.Text;
+            string name = Path.GetFileName(SourceFilePath);           
+
+            OutFile = @"./加密后的文件/encrypt-" + name; //组装新名字
+
+            Encrypt encrypt = new Encrypt(EncryptClass.EncryptDES);
+
+            encrypt.BeginInvoke(SourceFilePath, OutFile, "75205329", new AsyncCallback(encryptCompleted),null);
+            labelState.Text = "开始加密,请稍等";
+        }
+
+
+        //异步加密需要的委托
+        delegate bool Encrypt(string SourceFilePath, string OutFile, string key);
+
+        /// <summary>
+        /// 异步线程的回调
+        /// </summary>
+        /// <param name="result"></param>
+        private void encryptCompleted(IAsyncResult result)
+        {
+            
+            AsyncResult _result = (AsyncResult)result;
+            Encrypt insertDelegate = (Encrypt)_result.AsyncDelegate;
+            bool resultState = insertDelegate.EndInvoke(_result);
+            string addMac = (string)result.AsyncState;
+            if (resultState)
+            {
+                labelState.Text = "加密成功";
+                textBox1.Clear();
+                textBox1.Text = Path.GetFullPath(OutFile);
+            }
+            else
+            {
+                MessageBox.Show("加密失败");
+            }
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            textBox1.Text = openFileDialog1.FileName;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //取消线程间检查
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string SourceFilePath = @textBox1.Text;
+            string name = Path.GetFileName(SourceFilePath);
+
+            string OutFile = @"./解密后的文件/decrypt-" + name; //组装新名字
+
+            Encrypt decrypt = new Encrypt(EncryptClass.DecryptDES);
+
+            decrypt.BeginInvoke(SourceFilePath, OutFile, "75205329", new AsyncCallback(decryptCompleted), null);
+            labelState.Text = "开始解密，请稍等";
+        }
+
+        /// <summary>
+        /// 异步线程的回调
+        /// </summary>
+        /// <param name="result"></param>
+        private void decryptCompleted(IAsyncResult result)
+        {
+
+            AsyncResult _result = (AsyncResult)result;
+            Encrypt insertDelegate = (Encrypt)_result.AsyncDelegate;
+            bool resultState = insertDelegate.EndInvoke(_result);
+            string addMac = (string)result.AsyncState;
+            if (resultState)
+            {
+                labelState.Text = "解密成功";
+            }
+            else
+            {
+                MessageBox.Show("解密失败，请选择正确的加密文件");
+            }
         }
     }
 }
